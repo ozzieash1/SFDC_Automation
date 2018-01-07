c1163f892efb0b4808f9cc6001481b512baec57a
uashraf01 <ulfat.a.ashraf1@gmail.com>
2018-01-06 21:17:13 -0600

    Runner

--- Nov_Automation/src/test/java/runners/SalesForceRunner.java
+++ Nov_Automation/src/test/java/runners/SalesForceRunner.java
@@ -1,21 +1,20 @@
 package runners;
 
-import org.junit.runner.RunWith; 
+import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
- 
+
 @RunWith(Cucumber.class)
 
-@CucumberOptions(format = {"pretty", "html:target/cucumber-html-report", "json:target/cucumber.json"},
-                        features = {"src/test/resources/features" },
-                        glue = { "sfdc.automation.steps" },
-                        tags = {"@current"}
-                        //monochrome = true ,
-                       // dryRun = false
-                        )
-                        
-                        
+@CucumberOptions(format = { "pretty", "html:target/cucumber-html-report", "json:target/cucumber.json" }, features = {
+		"src/test/resources/features" }, glue = { "sfdc.automation.steps" },
+		tags = {"@currentt"}
+		//tags = { "@runme " }
+        // tags = { "@runme3" }
+// monochrome = true ,
+// dryRun = false
+)
+
 public class SalesForceRunner {
 
-    }
-
+}
